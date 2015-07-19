70bf35668d1168792c3bfa46abf09ddbc7bc31f2
Luca Briguglia <luca.briguglia@outlook.com>
2015-07-19 19:48:04 +0100

    Article code

---
+++ src/LB/Controllers/FilesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace LB.Controllers
+{
+    public class FilesController : ApiController
+    {
+        [HttpPost]
+        public async Task<IHttpActionResult> Post()
+        {
+            try
+            {
+                if (!Request.Content.IsMimeMultipartContent())
+                {
+                    return BadRequest("Not Mime Multipart Content");
+                }
+
+                var provider = new MultipartMemoryStreamProvider();
+
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                var filesToAttach = new Dictionary<string, byte[]>();
+                var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Upload");
+
+                foreach (var file in provider.Contents)
+                {
+                    var fileName = Path.GetFileName(file.Headers.ContentDisposition.FileName.Trim('\"'));
+                    var fullPath = Path.Combine(path, fileName);
+                    var buffer = await file.ReadAsByteArrayAsync();
+                    File.WriteAllBytes(fullPath, buffer);
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
